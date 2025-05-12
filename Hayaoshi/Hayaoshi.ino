5fa5710a3a7e37219e1611d6345ed7251e043507
ryota_miyagawa <i31640.ryota@gmail.com>
2025-05-12 15:34:51 +0900

    マジックナンバーを廃止

--- Hayaoshi/Hayaoshi.ino
+++ Hayaoshi/Hayaoshi.ino
@@ -1,8 +1,16 @@
-int button1 = 2;
-int button2 = 4;
-int outputPin = 9;
-int led1 = 10;
-int led2 = 11;
+// 型定義
+typedef unsigned char u1;
+
+// Pin番号
+u1 button1 = 2;
+u1 button2 = 4;
+u1 outputPin = 9;
+u1 led1 = 10;
+u1 led2 = 11;
+
+// ボタン押下時に電圧が下がるため、BUTTON_ONが0でBUTTON_OFFが1とする
+const u1 BUTTON_ON = 0;
+const u1 BUTTON_OFF = 1;
 
 int buttonFlag;
 
@@ -13,7 +21,7 @@ void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
 
-  buttonFlag = 0;
+  buttonFlag = BUTTON_OFF;
 
   digitalWrite(outputPin, HIGH);
 
@@ -25,20 +33,20 @@ void loop() {
   int button1State = digitalRead(button1);
   int button2State = digitalRead(button2);
 
-  if (buttonFlag == 1) return;
+  if (buttonFlag == BUTTON_ON) return;
   
-  if (button1State == 0 && button2State ==0){
+  if (button1State == BUTTON_ON && button2State == BUTTON_ON){
     digitalWrite(led1, HIGH);
     digitalWrite(led2, HIGH);
-    buttonFlag = 1;
+    buttonFlag = BUTTON_ON;
   }
-  else if (button1State == 0){
+  else if (button1State == BUTTON_ON){
     digitalWrite(led1, HIGH);
-    buttonFlag = 1;
+    buttonFlag = BUTTON_ON;
   }
-  else if (button2State == 0){
+  else if (button2State == BUTTON_ON){
     digitalWrite(led2, HIGH);
-    buttonFlag = 1;
+    buttonFlag = BUTTON_ON;
   }
   
   delay(1);
